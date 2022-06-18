7aa395648d34a4f0d37d3b0f3dc6167f25b2522a
crimson86 <nderitudaniel8@gmail.com>
2022-06-18 19:33:29 +0300

    s

--- I4G0158688H0/blog/models.py
+++ I4G0158688H0/blog/models.py
@@ -10,4 +10,4 @@ class Post(models.Model):
     published_date=models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-            return self.title + ' | ' +  self.author
+            return self.title + ' | ' +  str(self.author)
